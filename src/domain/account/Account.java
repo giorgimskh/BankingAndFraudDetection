5d0dfed1064bcc68690551b22e9afc0fcd90ed42
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-19 22:41:30 +0400

    implemented deposit method and also getters/setters

--- src/domain/account/Account.java
+++ src/domain/account/Account.java
@@ -1,19 +1,20 @@
 package domain.account;
 
 import domain.customer.Customer;
+import exception.CurrencyMismatchException;
 import util.Currency;
 import util.Money;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public class Account {
-    UUID id;
-    Customer owner;
-    Money balance;
-    Currency currency;
-    AccountStatus accountStatus;
-    AccountType accountType;
+public abstract class  Account {
+    private final UUID id;
+    private final Customer owner;
+    private Money balance;
+    private final Currency currency;
+    private AccountStatus accountStatus;
+    private AccountType accountType;
 
     public Account(Customer owner, Money balance, Currency currency, AccountStatus accountStatus, AccountType accountType) {
         if(owner==null)
@@ -45,6 +46,50 @@ public class Account {
         this.accountStatus = accountStatus;
         this.accountType = accountType;
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public Money getBalance() {
+        return balance;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public void deposit(Money money) throws CurrencyMismatchException {
+        if(this.accountStatus.equals(AccountStatus.CLOSED))
+            throw new IllegalStateException("Account must not be closed");
+
+        if(money.getCurrency()!=this.currency)
+            throw new CurrencyMismatchException("Currencies mismatched");
+
+        this.balance.add(money);
+    }
+
+
 
 
 }
