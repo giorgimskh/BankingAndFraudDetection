7eb9346f31fcd284257731577014a408a8f12c10
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-19 17:24:26 +0400

    implemented essential variables/lists of customer class

--- src/domain/customer/Customer.java
+++ src/domain/customer/Customer.java
@@ -1,5 +1,15 @@
 package domain.customer;
 
+import domain.account.Account;
+import domain.card.Card;
+
+import java.util.List;
+import java.util.UUID;
+
 public class Customer {
+    UUID id;
+    String fullName;
+    List<Account> accounts;
+    List<Card> cards;
 
 }
