6f71bff9d71166b15ad2d99ee467241b36d83194
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-19 21:58:13 +0400

    created Account class and constructor method

--- src/domain/account/Account.java
+++ src/domain/account/Account.java
@@ -1,4 +1,50 @@
 package domain.account;
 
+import domain.customer.Customer;
+import util.Currency;
+import util.Money;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class Account {
+    UUID id;
+    Customer owner;
+    Money balance;
+    Currency currency;
+    AccountStatus accountStatus;
+    AccountType accountType;
+
+    public Account(Customer owner, Money balance, Currency currency, AccountStatus accountStatus, AccountType accountType) {
+        if(owner==null)
+            throw new IllegalArgumentException("owner cant be null");
+        if(balance.getCurrency()!=currency)
+            throw new IllegalArgumentException("Currencies does not match");
+        if(accountStatus==null)
+            throw new IllegalArgumentException("account status cant be null");
+
+        this.id=UUID.randomUUID();
+        this.owner=owner;
+        this.currency=currency;
+        this.accountType=accountType;
+        this.accountStatus=accountStatus;
+    }
+
+    public Account(UUID id, Customer owner,  Currency currency, AccountStatus accountStatus, AccountType accountType) {
+        if(owner==null)
+            throw new IllegalArgumentException("owner cant be null");
+        if(balance.getCurrency()!=currency)
+            throw new IllegalArgumentException("Currencies does not match");
+        if(accountStatus==null)
+            throw new IllegalArgumentException("account status cant be null");
+
+        this.id = id;
+        this.owner = owner;
+        this.balance = new Money(BigDecimal.ZERO,currency);
+        this.currency = currency;
+        this.accountStatus = accountStatus;
+        this.accountType = accountType;
+    }
+
+
 }
