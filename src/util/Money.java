daad2875cc3c7414364bf2b81969bfc8b9d9c891
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-19 18:18:50 +0400

    implemented constructor in Money class

--- src/util/Money.java
+++ src/util/Money.java
@@ -1,4 +1,23 @@
 package util;
 
-public class Money {
+import java.math.BigDecimal;
+
+public class Money  implements  Comparable<Money>{
+    private final BigDecimal amount;
+    private final Currency currency;
+
+    public Money(BigDecimal amount, Currency currency) {
+        if(amount==null)
+            throw new IllegalArgumentException("Amount cannot be null");
+        if(currency==null)
+            throw new IllegalArgumentException("Currency cannot be null");
+
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    @Override
+    public int compareTo(Money o) {
+        return 0;
+    }
 }
