b536ebd109077a8fbda07e787731785c88f0c52d
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-19 23:13:21 +0400

    implementing canWithdraw method

--- src/domain/account/CheckingAccount.java
+++ src/domain/account/CheckingAccount.java
@@ -1,28 +1,47 @@
 package domain.account;
 
 import domain.customer.Customer;
+import exception.CurrencyMismatchException;
 import util.Currency;
 import util.Money;
 
 
 public class CheckingAccount extends Account {
-    Money overdraftLimit;
+    private final Money overdraftLimit;
 
     public CheckingAccount(Customer owner, Money balance, Currency currency, AccountStatus accountStatus, AccountType accountType,Money overdraftLimit) {
         super(owner, balance, currency, accountStatus, accountType);
+        validateOverdraft(overdraftLimit,currency);
         this.overdraftLimit=overdraftLimit;
     }
 
     public CheckingAccount(Customer owner, Currency currency, AccountStatus accountStatus, AccountType accountType,Money overdraftLimit) {
         super(owner, currency, accountStatus, accountType);
+        validateOverdraft(overdraftLimit,currency);
         this.overdraftLimit=overdraftLimit;
     }
 
-    @Override
-    protected boolean canWithdraw(Money amount) {
-        if(this.getBalance().getAmount().add(this.overdraftLimit.getAmount()).compareTo(amount.getAmount())>=0)
-            return true;
+    private void validateOverdraft(Money overdraftLimit, Currency currency) {
+        if (overdraftLimit == null)
+            throw new IllegalArgumentException("Overdraft limit cannot be null");
 
-        return false;
+        if (!overdraftLimit.getCurrency().equals(currency))
+            throw new IllegalArgumentException("Overdraft currency mismatch");
+
+        if (overdraftLimit.isNegative())
+            throw new IllegalArgumentException("Overdraft cannot be negative");
+    }
+
+    @Override
+    protected boolean canWithdraw(Money amount) throws CurrencyMismatchException {
+        if(amount==null)
+            throw new IllegalArgumentException("Amount cannot be null");
+
+        Money available=getOverdraftLimit().addMoney(overdraftLimit);
+        return available.compareTo(amount)>=0;
+    }
+
+    public Money getOverdraftLimit() {
+        return overdraftLimit;
     }
 }
