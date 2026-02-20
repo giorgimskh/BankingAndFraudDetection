09202b46594fc716182b3d4c5489949215f0b75d
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-21 00:45:47 +0400

    CREATED  and implemented Transaction.java

--- src/domain/transaction/Transaction.java
+++ src/domain/transaction/Transaction.java
@@ -6,17 +6,79 @@ import java.time.Instant;
 import java.util.UUID;
 
 public abstract class Transaction {
-    UUID id;
-    Instant createdAt;
-    Money amount;
-    TransactionStatus status;
-    String description;
+    private final UUID id;
+    private final Instant createdAt;
+    private final Money amount;
+    private  TransactionStatus status;
+    private final String description;
 
-    public Transaction(UUID id, Instant createdAt, Money amount, TransactionStatus status, String description) {
-        this.id = id;
-        this.createdAt = createdAt;
+    public Transaction(Money amount, TransactionStatus status, String description) {
+        if(amount==null)
+            throw new IllegalStateException("Amount cannot be null");
+        if(!amount.isPositive())
+            throw new IllegalStateException("Amount must be positive!");
+
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
         this.amount = amount;
-        this.status = status;
+        this.status = TransactionStatus.CREATED;
         this.description = description;
+    }
+
+    public abstract TransactionType type();
+    public abstract void apply();
+
+    public void approve(){
+        canBeModified();
+        if(this.status!=TransactionStatus.CREATED)
+            throw new IllegalStateException("This Transaction cannot be approved!");
+        this.status=TransactionStatus.APPROVED;
+    }
+
+    public void MarkReview(){
+        canBeModified();
+        if(status != TransactionStatus.CREATED)
+            throw new IllegalStateException("Only Created transaction can be  marked REVIEW!");
+        this.status = TransactionStatus.REVIEW;
+    }
+
+    public void decline(){
+        canBeModified();
+        if(this.status!=TransactionStatus.CREATED)
+            throw new IllegalStateException("Transaction is not created!It cant be declined!");
+        this.status=TransactionStatus.DECLINED;
+    }
+
+    public void markPosted(){
+        canBeModified();
+        if(this.status!=TransactionStatus.APPROVED)
+            throw new IllegalStateException("Only approved transaction are marked as posted!");
+        this.status=TransactionStatus.POSTED;
+    }
+
+    public void canBeModified(){
+        if(this.status==TransactionStatus.POSTED) {
+            throw new IllegalStateException("Transaction  cannot be modified");
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
