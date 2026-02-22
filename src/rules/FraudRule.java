f55b2afd71bff6205871d7d52ab97fe45fb725c4
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-23 00:45:03 +0400

    added exception

--- src/rules/FraudRule.java
+++ src/rules/FraudRule.java
@@ -1,7 +1,8 @@
 package rules;
 
 import domain.transaction.Transaction;
+import exception.CurrencyMismatchException;
 
 public interface FraudRule {
-    RuleResult evaluate(Transaction tx, FraudContext ctx);
+    RuleResult evaluate(Transaction tx, FraudContext ctx) throws CurrencyMismatchException;
 }
