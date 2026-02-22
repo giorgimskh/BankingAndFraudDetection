aa18a50be70c88527ae2dd6c2de5e5f8e5e3c520
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-22 18:32:15 +0400

    corrected few bugs

--- src/rules/RuleResult.java
+++ src/rules/RuleResult.java
@@ -5,37 +5,42 @@ public class RuleResult {
     private final String reason;
 
 
-    public RuleResult(Decision decision, String reason) {
+    private RuleResult(Decision decision, String reason) {
         if(decision==null)
             throw new IllegalArgumentException("Decision cant be null");
-        if(decision!=Decision.ALLOW && reason.isEmpty())
-            throw new IllegalArgumentException("Reason cant be empty");
+        if(decision==Decision.ALLOW){
+            this.reason=(reason==null) ? "":reason;
+        }else{
+            if(reason==null || reason.trim().isEmpty()){
+                throw new IllegalArgumentException("Reason cant be empty for "+decision);
+            }
+            this.reason=reason;
+        }
 
         this.decision = decision;
-        this.reason = reason;
     }
 
     public static RuleResult allow(){
         return new RuleResult(Decision.ALLOW,"");
     }
 
-    public static RuleResult review(){
-            return new RuleResult(Decision.REVIEW,"");
+    public static RuleResult review(String reason){
+            return new RuleResult(Decision.REVIEW,reason);
     }
 
-    public static RuleResult block(){
-        return new RuleResult(Decision.BLOCK, block().getReason());
+    public static RuleResult block(String reason){
+        return new RuleResult(Decision.BLOCK, reason);
     }
 
-    public static boolean isAllow(RuleResult result){
+    public  boolean isAllow(RuleResult result){
         return result.decision==Decision.ALLOW;
     }
 
-    public static boolean isReview(RuleResult result){
+    public  boolean isReview(RuleResult result){
         return result.decision==Decision.REVIEW;
     }
 
-    public static boolean isBlock(RuleResult result){
+    public  boolean isBlock(RuleResult result){
         return result.decision==Decision.BLOCK;
     }
 
