81f1e060c60b01112820d8dd2520f5e02131d28e
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-22 18:15:09 +0400

    Implemented RuleResult along with constructor and allow method

---
+++ src/rules/RuleResult.java
@@ -0,0 +1,22 @@
+package rules;
+
+public class RuleResult {
+    private final Decision decision;
+    private final String reason;
+
+
+    public RuleResult(Decision decision, String reason) {
+        if(decision==null)
+            throw new IllegalArgumentException("Decision cant be null");
+        if(decision!=Decision.ALLOW && reason.isEmpty())
+            throw new IllegalArgumentException("Reason cant be empty");
+
+        this.decision = decision;
+        this.reason = reason;
+    }
+
+    public static RuleResult allow(){
+        return new RuleResult(Decision.ALLOW,"");
+    }
+
+}
