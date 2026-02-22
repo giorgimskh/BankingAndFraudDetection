dcefd80d846f90051f3a03961af83ff4141eb16b
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-22 22:48:45 +0400

    adjusted few things

--- src/rules/FraudContext.java
+++ src/rules/FraudContext.java
@@ -9,12 +9,29 @@ import util.Money;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class FraudContext {
     private final Customer customer;
     private final List<Transaction> postedHistory;
     private final Instant now;
+
+    public FraudContext(Customer customer, List<Transaction> postedHistory, Instant now) {
+        if(customer==null)
+            throw new IllegalArgumentException("Customer field cant be null");
+        if(postedHistory==null)
+            throw new IllegalArgumentException("PostedHistory cant be null");
+        if(now==null)
+            throw new IllegalArgumentException("time cant be null");
+
+        this.customer = customer;
+        this.now = now;
+
+        List<Transaction> copy = new ArrayList<>(postedHistory);
+        copy.sort(Comparator.comparing(Transaction::getCreatedAt));
+        this.postedHistory = List.copyOf(copy);
+    }
 
     public FraudContext(Customer customer, List<Transaction> postedHistory) {
         if(customer==null)
@@ -23,8 +40,11 @@ public class FraudContext {
             throw new IllegalArgumentException("PostedHistory cant be null");
 
         this.customer = customer;
-        this.postedHistory = List.copyOf(postedHistory);
         this.now = Instant.now();
+
+        List<Transaction> copy = new ArrayList<>(postedHistory);
+        copy.sort(Comparator.comparing(Transaction::getCreatedAt));
+        this.postedHistory = List.copyOf(copy);
     }
 
     public List<Transaction> lastN(int n){
@@ -34,7 +54,7 @@ public class FraudContext {
         int size= getPostedHistory().size();
         int from=Math.max(0,size-n);
 
-        return postedHistory.subList(from,size);
+        return List.copyOf(postedHistory.subList(from,size));
     }
 
     public List<Transaction> withinMinutes(int minutes){
