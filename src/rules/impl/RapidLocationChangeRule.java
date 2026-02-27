8dce90e4576555b254cb5b3762a4f716d42eb605
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-27 23:45:55 +0400

    created RapidLocationChangeRule

--- src/rules/impl/RapidLocationChangeRule.java
+++ src/rules/impl/RapidLocationChangeRule.java
@@ -1,4 +1,33 @@
 package rules.impl;
 
+import rules.Decision;
+import rules.RuleResult;
+
+import java.time.Duration;
+
 public class RapidLocationChangeRule {
+    private Duration maxTimeBetween;
+    private final Decision decisionOnHit;
+
+    public RapidLocationChangeRule(Duration maxTimeBetween, Decision decisionOnHit) {
+        if(maxTimeBetween==null)
+            throw new IllegalArgumentException("maxTimeBetween cant be null");
+        if(decisionOnHit==null)
+            throw new IllegalArgumentException("decisionOnHit cant be null");
+        if(decisionOnHit==Decision.ALLOW)
+            throw new IllegalArgumentException("decisionOnHit cant be allow");
+        if(!maxTimeBetween.isPositive())
+            throw new IllegalArgumentException("maxTimeBetween must be positive");
+
+        this.maxTimeBetween = maxTimeBetween;
+        this.decisionOnHit = decisionOnHit;
+    }
+
+    public Duration getMaxTimeBetween() {
+        return maxTimeBetween;
+    }
+
+    public Decision getDecisionOnHit() {
+        return decisionOnHit;
+    }
 }
