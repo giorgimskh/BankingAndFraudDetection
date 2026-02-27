488bb587018fb4b8cf383e5a3e721ac219242c38
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-27 16:26:23 +0400

    created DailySpendLimitRule class

--- src/rules/impl/DailySpendLimitRule.java
+++ src/rules/impl/DailySpendLimitRule.java
@@ -1,4 +1,16 @@
 package rules.impl;
 
+import rules.Decision;
+
 public class DailySpendLimitRule {
+    private final Decision decisionOnHit;
+
+    public DailySpendLimitRule(Decision decisionOnHit) {
+        if(decisionOnHit==null)
+            throw new IllegalArgumentException("DecisionOnHit cant be null");
+        if(decisionOnHit==Decision.ALLOW)
+            throw new IllegalArgumentException("DecisionOnHit cant be Allow");
+
+        this.decisionOnHit = decisionOnHit;
+    }
 }
