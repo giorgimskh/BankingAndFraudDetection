d5f28bb1c361778899f57925f759d0f428e6cd04
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-28 00:34:59 +0400

    implemented evaluate method

--- src/rules/impl/RapidLocationChangeRule.java
+++ src/rules/impl/RapidLocationChangeRule.java
@@ -1,12 +1,18 @@
 package rules.impl;
 
+import domain.transaction.CardPayment;
+import domain.transaction.Transaction;
+import exception.CurrencyMismatchException;
 import rules.Decision;
+import rules.FraudContext;
+import rules.FraudRule;
 import rules.RuleResult;
 
 import java.time.Duration;
+import java.util.List;
 
-public class RapidLocationChangeRule {
-    private Duration maxTimeBetween;
+public class RapidLocationChangeRule implements FraudRule {
+    private final Duration maxTimeBetween;
     private final Decision decisionOnHit;
 
     public RapidLocationChangeRule(Duration maxTimeBetween, Decision decisionOnHit) {
@@ -29,5 +35,33 @@ public class RapidLocationChangeRule {
 
     public Decision getDecisionOnHit() {
         return decisionOnHit;
+    }
+
+    @Override
+    public RuleResult evaluate(Transaction tx, FraudContext ctx) throws CurrencyMismatchException {
+        if(!(tx instanceof CardPayment cp))
+            return RuleResult.allow();
+        if(ctx.getPostedHistory().isEmpty())
+            return RuleResult.allow();
+
+        List<Transaction> reversedList=ctx.getPostedHistory().reversed();
+        for (Transaction transaction: reversedList){
+            if(transaction instanceof CardPayment cardPayment){
+                if (!cardPayment.getMerchant().getCountry().equals(cp.getMerchant().getCountry())) {
+                    Duration difference = Duration.between(
+                            cardPayment.getCreatedAt(),
+                            cp.getCreatedAt()
+                    );
+
+                    if (!difference.isNegative() && difference.compareTo(maxTimeBetween) <= 0) {
+                        String msg = "Different country spotted";
+                        return decisionOnHit == Decision.REVIEW ? RuleResult.review(msg) : RuleResult.block(msg);
+                    }
+                }
+                return RuleResult.allow();
+            }
+        }
+
+        return RuleResult.allow();
     }
 }
