efee2106d9955786a3d4db6adb3eb76db2276f43
Giorgi <giorgimsxrikadze@gmail.com>
2026-02-28 15:07:21 +0400

    Implemented constructor

--- src/rules/FraudPipeline.java
+++ src/rules/FraudPipeline.java
@@ -1,4 +1,15 @@
 package rules;
 
+import java.util.List;
+
 public class FraudPipeline {
+    private final List<FraudRule> rules;
+
+    public FraudPipeline(List<FraudRule> rules) {
+        if(rules==null)
+            throw new IllegalArgumentException("rules can't be null");
+        if(rules.contains(null))
+            throw new IllegalArgumentException("Rules list should not contain null");
+        this.rules = List.copyOf(rules);
+    }
 }
